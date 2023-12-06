70f48f5f8b58e600df478a9df514c7b80d5d4cfb
JhoZzel <joel.lol177@gmail.com>
2023-12-06 09:57:20 -0500

    finish

---
+++ PracticaCalificada5/BDD-Cucumber/features/step_definitions/movie_steps.rb
@@ -0,0 +1,72 @@
+# movie_steps.rb
+# Add a declarative step here for populating the DB with movies.
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    Movie.create!(movie)
+    # you should arrange to add that movie to the database here.
+  end
+end
+
+Then /(.*) seed movies should exist/ do |n_seeds|
+  expect(Movie.count).to eq n_seeds.to_i
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  expect(page.body.index(e1) < page.body.index(e2)).to be_truthy
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(', ')
+  ratings.each do |rating|
+    if uncheck.nil?
+      check("ratings_#{rating}")
+    else
+      uncheck("ratings_#{rating}")
+    end
+  end
+end
+
+# Part 2, Step 3
+Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
+  # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
+  movies = movie_list.split(', ').map { |m| m.strip }
+  movies.each do |movie|
+    if no.nil?
+      expect(page).to have_content(movie)
+    else
+      expect(page).not_to have_content(movie)
+    end
+  end
+end
+
+Then /I should see all the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  expect(page).to have_css('table#movies tbody tr', count: Movie.count)
+end
+
+### Utility Steps Just for this assignment.
+
+Then /^debug$/ do
+  # Use this to write "Then debug" in your scenario to open a console.
+   require "byebug"; byebug
+  1 # intentionally force debugger context in this method
+end
+
+Then /^debug javascript$/ do
+  # Use this to write "Then debug" in your scenario to open a JS console
+  page.driver.debugger
+  1
+end
