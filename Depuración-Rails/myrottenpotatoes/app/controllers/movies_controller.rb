ef734c5e339f954101963f60b37891b2bb349347
JhoZzel <joel.lol177@gmail.com>
2023-12-13 09:21:41 -0500

    Modificacion

--- Depuración-Rails/myrottenpotatoes/app/controllers/movies_controller.rb
+++ Depuración-Rails/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -8,6 +8,10 @@ class MoviesController < ApplicationController
     
     @movie = Movie.find(id) # look up movie by unique ID
     logger.debug("movie.inspect() :  #{@movie.inspect}")
+    # Modificacion
+    logger.debug("Nueva linea añadida en la rama")
+    logger.debug("Nueva linea añadida en la rama")
+
     
     rescue => excep
       logger.debug("exception :  #{excep.message}")
